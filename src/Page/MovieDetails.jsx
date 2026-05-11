ea864a9e4b9ae263fd8de0c5b3c1863556267b3a
Cristi <cristibologa98@yahoo.com>
2026-05-11 15:06:37 +0300

    movie details

--- src/Page/MovieDetails.jsx
+++ src/Page/MovieDetails.jsx
@@ -1,50 +1,64 @@
-import img from "../assets/Money_Heist.png";
-import poster from "../assets/poster.png";
+import { useParams } from "react-router";
 import star from "../assets/star.svg";
 
 import styles from "./MovieDetails.module.scss";
 
 export default function MovieDetails({ data }) {
-  console.log(data);
+  const { id } = useParams();
+  const selectedMovie = data?.find((movie) => movie.id === Number(id));
+  if (!selectedMovie) {
+    return <div className={styles.loading}>Loading movie details...</div>;
+  }
+
+  const {
+    name,
+    vote_average,
+    title,
+    poster_path,
+    backdrop_path,
+    overview,
+    media_type,
+    release_date,
+    original_language,
+  } = selectedMovie;
+
+  const poster = `https://image.tmdb.org/t/p/original/${poster_path}`;
+
+  const backdrop = `https://image.tmdb.org/t/p/original/${backdrop_path}`;
+
+  // console.log(title);
+
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.poster}>
-          <img src={poster} alt="" />
+          <img src={backdrop} alt="" />
         </div>
         <div className={styles.title}>
           <span>MaileHereko / Movies</span>
-          <h3 className="h3">Avengers: Endgame</h3>
+          <h3 className="h3">{name || title}</h3>
         </div>
       </div>
 
       <div className={styles.main}>
         <div className={`${styles.image}  `}>
-          <img src={img} width={480} alt="" />
+          <img src={poster} width={480} alt="" />
         </div>
 
         <div className={styles.box}>
-          <h4 className="h4">Part of the journey is the end.</h4>
-          <p className="p">
-            After the devastating events of Avengers: Infinity War, the universe
-            is in ruins due to the efforts of the Mad Titan, Thanos. With the
-            help of remaining allies, the Avengers must assemble once more in
-            order to undo Thanos' actions and restore order to the universe once
-            and for all, no matter what consequences may be in store.
-          </p>
+          <h4 className="h4">{name || title}</h4>
+          <p className="p">{overview}</p>
           <div className={styles.rating}>
             <img src={star} alt="" />
-            8.3
+            {Number(vote_average.toFixed(1))}
           </div>
           <ul className="list">
-            <li>type</li>
-            <li>Movie</li>
-            <li>release</li>
-            <li>2013.0.1.</li>
-            <li>Run time</li>
-            <li>181 min</li>
-            <li>Genres</li>
-            <li>adventure</li>
+            <li>Type</li>
+            <li>{media_type}</li>
+            <li>Release</li>
+            <li>{release_date}</li>
+            <li>Original Language</li>
+            <li>{original_language.toUpperCase()}</li>
           </ul>
         </div>
       </div>
